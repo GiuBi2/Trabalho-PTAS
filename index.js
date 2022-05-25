1c5ba5a9d3020084eafbcc4b4ca067bc37236e64
Wellen Pereira <86011091+WellenPereiira@users.noreply.github.com>
2022-05-25 21:31:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+var express = require("express");
+var app = express();
+var { autor } = require("./models")
+var { livro } = require("./models")
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// ROTAS PARA AUTOR
+app.get("/autores", async function(req, res) {
+  var resultado = await autor.findAll(req.body);
+  res.json(resultado);
+});
+
+app.get("/autores/:id", async function(req, res) {
+  var resultado = await autor.findOne({where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+// NOVA ROTA QUE MOSTRA O LIVRO DO AUTOR
+app.get("/autores/:id/livro", async function(req, res){
+  let resultado = await autor.findByPk(req.params.id, { 
+      include: 'livro' 
+  });
+  res.json(resultado.livro);
+});
+
+app.post("/autores", async function(req, res) {
+  var resultado = await autor.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/autores/:id", async function(req, res) {
+  var resultado = await autor.update(req.body, {where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+app.delete("/autores/:id", async function(req, res) {
+  var resultado = await autor.destroy({where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+// ROTAS PARA LIVROS
+app.get("/livros", async function(req, res) {
+  var resultado = await livro.findAll(req.body);
+  res.json(resultado);
+});
+
+app.get("/livros/:id", async function(req, res) {
+  var resultado = await livro.findOne({where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+// NOVA ROTA QUE MOSTRA TODOS OS LIVROS DE UM AUTOR
+app.get("/livros/:id/autores", async function(req, res){
+  let resultado = await livro.findByPk(req.params.id, { 
+      include: 'autores' 
+  });
+  res.json(resultado.autores);
+});
+
+app.post("/livros", async function(req, res) {
+  var resultado = await livro.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/livros/:id", async function(req, res) {
+  var resultado = await livro.update(req.body, {where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+app.delete("/livros/:id", async function(req, res) {
+  var resultado = await livro.destroy({where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+
+app.listen(3000, function() {
+  console.log("O servidor está legalzão, está bruto demais e fumegando");
+});
